d6a54b4de34e8bb09f6aea85812325e7214d8ca1
SayaniSen2022 <sayanisen.official@gmail.com>
2023-06-03 19:44:43 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let catButton = document.querySelector(".cat-btn");
+
+catButton.addEventListener("click", fetchPics);
+
+function fetchPics() {
+  let catsImgDiv = document.querySelector(".cat-img");
+  catsImgDiv.innerHTML = "";
+
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      let catsImgUrl = data[0].url;
+
+      let catsImgEl = document.createElement("img");
+      catsImgEl.setAttribute("src", `${catsImgUrl}`);
+      catsImgEl.classList.add("showcase");
+
+      let catsImgDiv = document.querySelector(".cat-img");
+      catsImgDiv.appendChild(catsImgEl);
+    })
+    .catch((err) => console.log(err));
+}
